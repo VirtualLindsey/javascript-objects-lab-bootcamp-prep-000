914fb39118bec8991a5831020431f257906f1361
L D <tapircode@gmail.com>
2019-02-16 23:41:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,3 +10,10 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   object[key] = value
   return object
 }
+
+function deleteFromObjectByKey(object, key){
+  var newObj = Object.assign({}, Object)
+  delete newObj[key]
+  return newObj
+  
+}
